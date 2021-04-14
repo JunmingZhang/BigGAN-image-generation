9ce276cadf285a14124a8228e157b683955c383d
Angelo Wong <chermissote@gmail.com>
2021-04-14 10:45:08 -0400

    Changed dataloader function

--- dataloaders/setup_dataloader_smallgan.py
+++ dataloaders/setup_dataloader_smallgan.py
@@ -5,7 +5,7 @@ from torch.utils.data import  DataLoader
 
 def setup_dataloader(name,h=128,w=128,batch_size=4,num_workers=4):
     '''
-    instead of setting up dataloader that read raw image from file, 
+    instead of setting up dataloader that read raw image from file,
     let's use store all images on cpu memmory
     because this is for small dataset
     '''
@@ -13,9 +13,11 @@ def setup_dataloader(name,h=128,w=128,batch_size=4,num_workers=4):
         img_path_list = glob.glob("./data/face/*.png")
     elif name=="anime":
         img_path_list = glob.glob("./data/anime/*.png")
+    elif name == "flowers":
+        img_path_list = glob.glob("./data/flowers/*.jpeg")
     else:
         raise NotImplementedError("Unknown dataset %s"%name)
-        
+
     assert len(img_path_list) > 0
 
     transform = transforms.Compose([
@@ -23,9 +25,9 @@ def setup_dataloader(name,h=128,w=128,batch_size=4,num_workers=4):
             transforms.CenterCrop( (h,w) ),
             transforms.ToTensor(),
             ])
-    
+
     img_path_list = [[path,i] for i,path in enumerate(sorted(img_path_list))]
     dataset = ImageListDataset(img_path_list,transform=transform)
-    
-    return  DataLoader([data for data in  dataset],batch_size=batch_size, 
+
+    return  DataLoader([data for data in  dataset],batch_size=batch_size,
                             shuffle=True,num_workers=num_workers)
